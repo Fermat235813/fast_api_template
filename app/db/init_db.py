3ebc4d067f16c5c80380221f735f9fa491349565
Fermat235813 <lothar.janssen82@icloud.com>
2025-01-05 13:23:32 +0100

    add database connection

---
+++ app/db/init_db.py
@@ -0,0 +1,11 @@
+import typing
+
+from fastapi import FastAPI
+
+
+def execute_backend_server_event_handler(app: FastAPI) -> typing.Any:
+    pass
+
+
+def terminate_backend_server_event_handler(app: FastAPI) -> typing.Any:
+    pass
---
+++ app/models.py
@@ -0,0 +1,2 @@
+from app.db.core_table import Base
+from app.auth.models import Account
